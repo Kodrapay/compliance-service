ac92ab254ffc61af787818eddc16786c582720cd
KodraPay Dev <dev@kodrapay.com>
2025-12-04 01:54:36 +0100

    Containerize

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,16 +3,9 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/kodra-pay/compliance-service/internal/handlers"
-	"github.com/kodra-pay/compliance-service/internal/services"
 )
 
-func Register(app *fiber.App, service string) {
-	health := handlers.NewHealthHandler(service)
+func Register(app *fiber.App, serviceName string) {
+	health := handlers.NewHealthHandler(serviceName)
 	health.Register(app)
-
-	svc := services.NewComplianceService()
-	h := handlers.NewComplianceHandler(svc)
-	api := app.Group("/audit")
-	api.Post("/logs", h.WriteAudit)
-	api.Get("/logs", h.ListAudit)
 }
